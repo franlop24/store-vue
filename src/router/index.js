b38ec85908bf4b75f29e018dc59db1d01baebd7c
Franlop24 <franlopbri@gmail.com>
2024-11-26 12:44:39 -0600

    01 Initial setup with tailwind

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import ShopView from '../views/ShopView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: ShopView,
+    }
+  ],
+})
+
+export default router
---
+++ src/views/ShopView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Shop View</h1>
+</template>
